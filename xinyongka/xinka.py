66ef9a8524ebaed48f283c8cf00b7bb6d420c2e4
hipac <gggiiiudx0308@gmail.com>
2016-08-04 17:18:10 +0800

    aa

--- xinyongka/xinka.py
+++ xinyongka/xinka.py
@@ -1,39 +1,73 @@
 __author__ = 'water'
-
+# import global.py
 def xf(money,cp):
-    if(x-money<0):
-         print 'yu e i zu ...'
+    global x
+    if x-money<0 :
+         print 'Sorry, your credit is running low...'
+         je=x
+         print '''Balance:%d  '''%(je)
+         return x
     else:
+        # xfmx(money,cp)
          x=x-money
-         xfmx(money,cp)
-    print '''ke yong yu e %s .'''%(x)
+         je=x
+         print ''' Balance:%d  '''%(je)
+         return x
 
-def hk(money,cp):
-  if(money<0):
+
+def hk(money):
+  global x
+  if money<0:
       print 'huan kuan jin e <0 '
   else:
+      # xfmx(money,cp)
       x=x+money
       print '''ke yong yu e %s .'''%(x)
-      xfmx(money,cp)
 
 def xfmx(money,cp):
-   mx.append(money,cp)
+   pass
+    # cc=cp
+    # zd[pp]  =money
 
-def cz(mx):
-    for j,k in mx:
-        print mx[j][k]
-def int():
-  products=[]
-  prices=[]
+def init():
+  zd={}
+  products={}
   f=open('shop1.txt','r')
-  # for i in f. readlines():
-  #   p=i.split()[0]
-  #   pri=i.split()[1]
-  #   products.append(p)
-  #   prices.append(pri)
+  for i in f.readlines():
+      pp=i.split()[0]
+      products[pp]  =i.split()[1]
+  return products
+aa=init()
+global x
+x=15000
+n=1
 
-def main():
-    int()
-    mx=[]
-    x=15000
-    a=input('please input you cp:')
+while x:
+    tp=input('please input you type '
+             '1:sale'
+             '2:zz'
+             '3:mx (select):')
+    if tp==1:
+            while 1:
+                a=raw_input('please input you cp:  please exit......')
+                if(a=='exit'):break
+                if a in aa.keys():
+                    price=int(aa.get(a))
+                    xf(price,a)
+                else:
+                    print 'without this product.. '
+
+    elif tp==2:
+            while 1:
+
+                a=int(raw_input('please input you zhizhi money:  please exit.....'))
+                if(a=='exit'):break
+
+                hk(a)
+    elif tp==3:
+        pass
+    else:
+        print 'Without this type'
+
+
+
